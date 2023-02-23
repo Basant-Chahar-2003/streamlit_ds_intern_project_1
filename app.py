d84040441437ba885302d8af79af1a580e9cdb40
Basant <118425415+Basant-Chahar-2003@users.noreply.github.com>
2023-02-23 18:21:53 +0530

    update text

--- app.py
+++ app.py
@@ -1,2 +1,2 @@
 import streamlit as st
-st.header("Hello World!")
+st.header("Hello Everyone This is my first streamlit web application under Innomatics Data Science Internship Program. I am very Excited to do more.")
